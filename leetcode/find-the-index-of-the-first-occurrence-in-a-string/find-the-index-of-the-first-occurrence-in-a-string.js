86ae2ee09e812226d4a944423a6aa02f48915007
individual-ism <individual-ism-code@proton.me>
2023-03-03 13:22:43 -0500

    Complete JavaScript solution to Find the Index of the First Occurrence in a String LeetCode problem

--- leetcode/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ leetcode/find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -6,5 +6,9 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
+    if (haystack.includes(needle)) {
+        return haystack.indexOf(needle)
+    } else {
+        return -1
+    }
 };
