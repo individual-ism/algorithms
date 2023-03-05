dfdd2f2a5c2000504f9f15356a2c45961e999d9a
individual-ism <individual-ism-code@proton.me>
2023-03-05 03:41:10 -0500

    Partial Python solution to First Bad Version LeetCode problem

--- leetcode/easy/first-bad-version/first-bad-version.py
+++ leetcode/easy/first-bad-version/first-bad-version.py
@@ -56,3 +56,68 @@ class Solution:
                 i += 2
             if isBadVersion(i + 1):
                 return i
+
+
+# Does not meet time requirements but functions correctly
+        earliestVersion = 1
+        latestVersion = n
+        midVersion = (latestVersion - earliestVersion) // 2
+
+        while earliestVersion < latestVersion:
+            if isBadVersion(midVersion) and not isBadVersion(midVersion - 1):
+                return midVersion
+            else:
+                if isBadVersion(midVersion) and isBadVersion(midVersion - 1):
+                    midVersion -=1
+                elif not isBadVersion(midVersion) and isBadVersion(midVersion + 1):
+                    return midVersion + 1
+                elif not isBadVersion(midVersion) and not isBadVersion(midVersion + 1):
+                    midVersion += 1
+                else:
+                    return i
+        return earliestVersion
+    
+
+
+# Testing
+        earliestVersion = 1
+        latestVersion = n
+        midVersion = (latestVersion - earliestVersion) // 2
+
+        while earliestVersion < latestVersion:
+            if isBadVersion(midVersion) and not isBadVersion(midVersion - 1):
+                return midVersion
+            elif isBadVersion(midVersion) and isBadVersion(midVersion - 1):
+                    midVersion -= 1
+            elif not isBadVersion(midVersion) and not isBadVersion(midVersion + 1):
+                midVersion += 1
+        return earliestVersion
+    
+
+                elif not isBadVersion(midVersion) and isBadVersion(midVersion + 1):
+                return midVersion + 1
+    
+
+
+# Does not work for 2,2
+        earliestVersion = 1
+        latestVersion = n
+
+        while earliestVersion < latestVersion:
+            if isBadVersion(latestVersion):
+                latestVersion -= 1
+            elif not isBadVersion(latestVersion) and isBadVersion(latestVersion + 1):
+                return latestVersion + 1
+        return latestVersion
+    
+# Works for 2,2 but times out
+        earliestVersion = 1
+        latestVersion = n
+
+        while earliestVersion <= latestVersion:
+            if isBadVersion(latestVersion) and not isBadVersion(latestVersion - 1):
+                return latestVersion
+            elif isBadVersion(latestVersion) and isBadVersion(latestVersion - 1):
+                latestVersion -= 1
+            elif not isBadVersion(latestVersion) and isBadVersion(latestVersion + 1):
+                return latestVersion + 1
