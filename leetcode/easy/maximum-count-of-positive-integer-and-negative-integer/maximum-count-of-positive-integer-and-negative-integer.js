a5a59c5fa4a3dc28b2206e3ee125065286e0d6b3
individual-ism <individual-ism-code@proton.me>
2023-04-14 02:35:16 -0400

    Complete JavaScript solution to Maximum Count of Positive Integer and Negative Integer LeetCode problem

---
+++ leetcode/easy/maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,24 @@
+/*
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+
+Note that 0 is neither positive nor negative.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    let pos = 0;
+    let neg = 0;
+    nums.forEach((num) => {
+        if (num > 0) {
+            pos += 1;
+        } else if (num < 0) {
+            neg += 1;
+        }
+    })
+    return Math.max(pos, neg);
+};
