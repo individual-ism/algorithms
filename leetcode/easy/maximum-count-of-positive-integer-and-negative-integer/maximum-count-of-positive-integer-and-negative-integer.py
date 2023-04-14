479c209e8262ff18592c299356c56c3f9b81df15
individual-ism <individual-ism-code@proton.me>
2023-04-14 02:26:20 -0400

    Complete Python solution to Maximum Count of Positive Integer and Negative Integer LeetCode problem

---
+++ leetcode/easy/maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -0,0 +1,18 @@
+'''
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+
+Note that 0 is neither positive nor negative.
+'''
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        neg = 0
+        pos = 0
+        for num in nums:
+            if num > 0:
+                pos += 1
+            elif num < 0:
+                neg += 1
+        return max(pos, neg)
