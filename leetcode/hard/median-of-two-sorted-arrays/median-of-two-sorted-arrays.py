71a22b2e154c7d558903d37e76d4440b88ccabf6
individual-ism <individual-ism-code@proton.me>
2023-04-17 11:21:07 -0400

    Provides more efficient Python solution to Median of Two Sorted Arrays LeetCode problem

--- leetcode/hard/median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ leetcode/hard/median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -20,3 +20,19 @@ class Solution:
                 return merged[lowIndex]
         return (merged[lowIndex] + merged[highIndex]) / 2
     
+# More efficient solution in both runtime and memory
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        for num in nums2:
+            nums1.append(num)
+        merged = sorted(nums1)
+        lowIndex, highIndex = 0, 1
+        if len(merged) == 1:
+            return merged[0]
+        while highIndex < len(merged) - 1:
+            lowIndex += 1
+            highIndex += 2
+        if len(merged) % 2 != 0:
+            return merged[lowIndex]
+        else:
+            return (merged[lowIndex] + merged[lowIndex + 1]) / 2
