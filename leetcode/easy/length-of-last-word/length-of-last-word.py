a889c3f36a3c9190377d792a138c0bf20bc0c661
individual-ism <individual-ism-code@proton.me>
2023-04-21 14:45:46 -0400

    Partial Python solution to Length of Last Word LeetCode problem

---
+++ leetcode/easy/length-of-last-word/length-of-last-word.py
@@ -0,0 +1,10 @@
+'''
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+'''
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        ind_words = s.split(' ')
+        return len(ind_words[-1]) 
