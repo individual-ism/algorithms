888256f363cae7626f30e181d6f6bd1594abd18c
individual-ism <individual-ism-code@proton.me>
2023-04-28 23:39:50 -0400

    Partial Python solution to Super Pow LeetCode problem

---
+++ leetcode/medium/super-pow/super-pow.py
@@ -0,0 +1,10 @@
+# Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        if a == 1: return 1
+        bString = ''
+        for i in b:
+            bString += str(i)
+        print(bString)
+        return a ** int(bString)
