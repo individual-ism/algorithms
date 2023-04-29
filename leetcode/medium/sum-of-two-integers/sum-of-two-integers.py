8505e9e298705163e4e775cf069ed58c7db76e82
individual-ism <individual-ism-code@proton.me>
2023-04-28 23:14:58 -0400

    Complete Python solution to Sum of Two Integers LeetCode problem

---
+++ leetcode/medium/sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,7 @@
+'''
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+'''
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return sum([a, b])
