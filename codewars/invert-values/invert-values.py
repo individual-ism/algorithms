37948f11a4e2c377aa8b59444f873a9edbde3015
individual-ism <individual-ism-code@proton.me>
2023-04-29 13:47:42 -0400

    Complete Python solution to Invert Values Codewars problem

---
+++ codewars/invert-values/invert-values.py
@@ -0,0 +1,15 @@
+'''
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+You can assume that all values are integers. Do not mutate the input array/list.
+'''
+
+def invert(lst):
+    values = []
+    for i in range(len(lst)):
+        values.append(lst[i] * -1)
+    return values
