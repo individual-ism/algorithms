b31620ba9eb5cff8216b4315917cc6276f5414ce
individual-ism <individual-ism-code@proton.me>
2023-06-02 19:00:09 -0400

    Additional Python solution to Arrays - DS HackerRank problem

--- HackerRank/arrays-ds/arrays-ds.py
+++ HackerRank/arrays-ds/arrays-ds.py
@@ -17,6 +17,9 @@ def reverseArray(a):
     # Write your code here
     return reversed(a)
 
+    # Alternative solution
+    # return a[::-1]
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
