5497f451585005ff3d148ce64fbe1a562b51c6e8
individual-ism <individual-ism-code@proton.me>
2023-08-01 00:36:38 -0400

    Alternative C++ solution to Maximum Multiple Codewars problem

--- codewars/maximum-multiple/maximum-multiple.cpp
+++ codewars/maximum-multiple/maximum-multiple.cpp
@@ -6,3 +6,9 @@ int maxMultiple(int divisor, int bound)
   }
   return bound;
 }
+
+int maxMultiple(int divisor, int bound) 
+{
+  // Your Code is Here ... Enjoy !!! 
+  return bound - bound % divisor;
+}
